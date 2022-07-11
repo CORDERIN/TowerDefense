7307e55b576fdb6051a67151176aeedeaebd1451
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-07-10 23:29:22 -0300

    add

---
+++ Tower-Defense-main/Assets/Scripts/Inimigo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        UnityEngine.AI.NavMeshAgent agente = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        GameObject fimDoCaminho = GameObject.Find ("FimDoCaminho");
+        Vector3 posicaoDoFimDoCaminho = fimDoCaminho.transform.position;
+        agente.SetDestination (posicaoDoFimDoCaminho);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
